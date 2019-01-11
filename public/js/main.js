356804226254953d1ddb0b955c1a7a1188dfa840
Lokit007 <it007bk@gmail.com>
2019-01-11 13:22:38 +0700

    Update event user

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,17 @@
 function AddNew() {
-    $('#cancel-new').show();
+    eventUser = 'NewData';
+    ShowHide(true);
     $('#cau-1').removeAttr('disabled');
     var btnAdd = $('#add-new');
     btnAdd.attr('onclick', 'AddData()');
     btnAdd.html('Cập nhật');
+    
 }
 
 function CancelAdd() {
+    eventUser = 'study';
+    ShowHide(false);
     $('#cau-1').attr('disabled', 'true');
-    $('#cancel-new').hide();
     var btnAdd = $('#add-new');
     btnAdd.attr('onclick', 'AddNew()');
     btnAdd.html('Thêm mới');
@@ -40,3 +43,96 @@ function AddData() {
         }
     });
 }
+
+function ShowData() {
+    if (data != {}) {
+        var index = Math.floor(Math.random() * data.length);
+        var dung = data[index].dung;
+        var sai = data[index].sai;
+
+        $('#so-lan-hoc').html((dung + sai));
+        $('#check-ok').html((dung));
+        $('#check-not-ok').html((sai));
+
+        $('#cau-1').val(data[index].lg1.cau);
+        $('#id-cau').val(data[index]._id);
+        $('#res-cau-1').val(data[index].lg2[0].cau);
+        $('#index').val(index);
+    }
+}
+
+function UpdateData() {
+    $('#check').attr('disabled', 'true');
+    var traLoi = $('#cau-2').val();
+    var dung = parseInt($('#check-ok').html());
+    var sai = parseInt($('#check-not-ok').html());
+    var index = $('#index').val();
+
+    if (traLoi == $('#res-cau-1').val()) dung += 1;
+    else sai += 1;
+
+    $.ajax({
+        url: '/update-cau',
+        method: 'POST',
+        data: {
+            cau: $('#id-cau').val(),
+            dung: dung,
+            sai: sai,
+        },
+        success: rs => {
+            $('#check-ok').html(dung);
+            $('#check-not-ok').html(sai);
+            $('#so-lan-hoc').html(dung + sai);
+            data[index].sai = parseInt($('#check-not-ok').html());
+            data[index].dung = parseInt($('#check-ok').html());
+            $('#check').removeAttr('disabled');
+            if (traLoi == $('#res-cau-1').val()) {
+                $('#status').html('Bạn đã trả lời đúng, cố gắng phát huy nhé! Xin chúc mừng!');
+                $('#status').attr('class', 'dung');
+            } else {
+                $('#status').html('Bạn đã trả lời sai mất rồi, cẩn thận nhé!');
+                $('#status').attr('class', 'sai');
+            }
+            ShowData();
+        },
+        error: err => {
+            console.log(err);
+            alert('Cập nhật dữ liệu thất bại!');
+            $('#check').removeAttr('disabled');
+        }
+    });
+}
+
+function onTestChange() {
+    var key = window.event.keyCode;
+    if (key === 13) {
+        if(eventUser == 'study') UpdateData();
+        else AddData();
+    }
+}
+
+function ShowHide(isShow) {
+    if(isShow) {
+        $('#cancel-new').show();
+        $('#status-home').show();
+        $('#check').hide();
+        $('#status').hide();
+        $('#study').hide();
+        $('#help').hide();
+    } else {
+        $('#cancel-new').hide();
+        $('#status-home').hide();
+        $('#check').show();
+        $('#status').show();
+        $('#study').show();
+        $('#help').show();
+    }
+}
+
+function HelpTest() {
+    $('#status').html($('#res-cau-1').val());
+}
+
+$(document).ready(() => {
+    ShowData();
+});
